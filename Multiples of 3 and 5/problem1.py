de920c94c213b70e0a4476e6baebef98211cc6de
Ted <ted.pinkerton@mail.utoronto.ca>
2021-01-12 12:13:51 -0500

    New versions in haskell

--- Multiples of 3 and 5/problem1.py
+++ Multiples of 3 and 5/problem1.py
@@ -2,5 +2,8 @@ limit = 1000
 factors = (3, 5)
 
 n = sum(x for x in range(0, limit, int(limit/abs(limit))) if any(x%factor == 0 for factor in factors))
+print(n)
 
+
+n = sum(set(range(3, 1001, 3))|set(range(5, 1001, 5)))
 print(n)
